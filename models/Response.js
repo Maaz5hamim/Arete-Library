af7676572f2fe095d34f4663873f541e25458cd0
Maaz Shamim <m.shamim.22792@khi.iba.edu.pk>
2024-04-15 03:10:32 +0500

    Response Checking Mechanism
    Automatically grades True/False & MCQ questions upon recording

--- models/Response.js
+++ models/Response.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Question = require('./Question')
 
 const responseSchema = new mongoose.Schema
 ({
@@ -15,8 +16,45 @@ const responseSchema = new mongoose.Schema
 }, 
 { timestamps: true })
 
+responseSchema.pre('save', function(next) {
+    const responses = this.responses
+    let totalScore = 0
+
+    responses.forEach(response => 
+    {
+        Question.findById(response.questionId, (err, question) => 
+        {
+            if (err) {
+                return next(err)
+            }
+            if (!question) {
+                return next(new Error('Question not found'))
+            }
+
+            if (question.type === 'True/False') {
+                if (response.answer[0] === 'false' && !question.isTrue || response.answer[0] === 'true' && question.isTrue) {
+                    response.score = question.points
+                    totalScore += question.points
+                }
+            } else if (question.type === 'MCQ') {
+                const selectedOptions = response.answer
+
+                const isAnswerCorrect = correctOptions.every(option => selectedOptions.includes(question.correctOptions))
+                if (isAnswerCorrect) {
+                    response.score = question.points
+                    totalScore += question.points
+                }
+            }
+
+            this.totalScore = totalScore
+
+            next()
+        })
+    })
+})
+
 responseSchema.index({ 'assessment': 1 })
 
-const Response = mongoose.model('Response', responseSchema);
+const Response = mongoose.model('Response', responseSchema)
 
-module.exports = Response;
+module.exports = Response
