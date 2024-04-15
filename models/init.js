040c30d1769afcf36c0e5744cd78c9258635cd08
Maaz Shamim <m.shamim.22792@khi.iba.edu.pk>
2024-04-15 23:33:07 +0500

    Create init.js
    This file would initiate all models

---
+++ models/init.js
@@ -0,0 +1,9 @@
+const Student = require('./Student')
+const Assessment = require('./Assessment')
+const Class = require('./Class')
+const Question = require('./Question')
+const Response = require('./Response')
+const Section = require('./Section')
+const Teacher = require('./Teacher')
+
+module,exports = {Student, Teacher, Assessment, Class, Section, Question, Response}
