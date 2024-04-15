6b6f33c6f9311d7d326100a323dae300a46bae57
Maaz Shamim <m.shamim.22792@khi.iba.edu.pk>
2024-04-15 23:43:35 +0500

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const Student = require('./Student')
+const Assessment = require('./Assessment')
+const Class = require('./Class')
+const Question = require('./Question')
+const Response = require('./Response')
+const Section = require('./Section')
+const Teacher = require('./Teacher')
+
 console.log('Welcome to Arete Library')
--- models/init.js
+++
@@ -1,9 +0,0 @@
-const Student = require('./Student')
-const Assessment = require('./Assessment')
-const Class = require('./Class')
-const Question = require('./Question')
-const Response = require('./Response')
-const Section = require('./Section')
-const Teacher = require('./Teacher')
-
-module,exports = {Student, Teacher, Assessment, Class, Section, Question, Response}
