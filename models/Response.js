4e819601b73416e8640240a870be525f195f4273
Maaz Shamim <m.shamim.22792@khi.iba.edu.pk>
2024-04-17 01:32:42 +0500

    Updated Response Model
    Included submission time

--- models/Response.js
+++ models/Response.js
@@ -5,6 +5,7 @@ const responseSchema = new mongoose.Schema
 ({
     assessment: { type: mongoose.Schema.Types.ObjectId, ref: 'Assessment', required: true }, 
     student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true }, 
+    submitted: Date,
     status: { type: String, enum: [ 'Active', 'Submitted'], default: 'Active' },  
     responses: 
     [{ 
